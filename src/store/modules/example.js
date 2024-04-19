548241a3fd7aba2d47f59358dbde66eabbd7cf5d
ctenetliu <ctenetliu@tencent.com>
2024-04-19 09:38:59 +0800

    feat: L2-基于CMDB实现游戏业务主机资源管理，实现基本界面以及后端交互

--- src/store/modules/example.js
+++ src/store/modules/example.js
@@ -17,9 +17,34 @@ export default {
       // eslint-disable-next-line no-undef
       return http.get(`/api/table?&${queryString.stringify(params)}`, params, config);
     },
+    // 查询业务列表
     getBizData(context, params, config = {}) {
       // eslint-disable-next-line no-undef
-      return http.get(`https://apps.ce.bktencent.com/stag--default--bk-scut-course/biz-list`, params, config);
+      return http.get(`http://dev.ce.bktencent.com:8000/biz-list`, params, config);
+    },
+    // 根据业务ID，查询集群列表
+    getSetData(context, params, config = {}) {
+      // eslint-disable-next-line no-undef
+      const url = `http://dev.ce.bktencent.com:8000/set-list?${queryString.stringify(params)}`;
+      return http.get(url, {}, config);
+    },
+    // 根据业务ID和集群ID，查询模块列表
+    getModuleData(context, params, config = {}) {
+      // eslint-disable-next-line no-undef
+      const url = `http://dev.ce.bktencent.com:8000/module-list?${queryString.stringify(params)}`;
+      return http.get(url, {}, config);
+    },
+    // 根据传入的查询参数，查询主机列表
+    getHostsData(context, params, config = {}) {
+      // eslint-disable-next-line no-undef
+      const url = `http://dev.ce.bktencent.com:8000/host-list?${queryString.stringify(params)}`;
+      return http.get(url, {}, config);
+    },
+    // 根据传入的主机ID，查询主机详情信息
+    getHostDetail(context, params, config = {}) {
+      // eslint-disable-next-line no-undef
+      const url = `http://dev.ce.bktencent.com:8000/host-detail?${queryString.stringify(params)}`;
+      return http.get(url, {}, config);
     },
   },
 };
