0589c102310f1adefb322ba619d821a591c7d5b2
ctenetliu <ctenetliu@tencent.com>
2024-04-26 14:29:08 +0800

    feat: BKVision

--- src/views/example1/index.vue
+++ src/views/example1/index.vue
@@ -2,7 +2,7 @@
   <div class="example1-wrapper" >
 
     <iframe
-        src="http://apps.ce.bktencent.com/bk-vision/embed/?uid=3mu6wqHieVcLFhAygQ3R5i&bk_app_id=&bk_app_list=['bk-scut-course']&name=&show_copyright=True&watermark=True&time_readonly=False&show_time=True&show_refresh=True&start_time=now/d&end_time=now/d&preview=False&hide_toolbox=False&hide_filter=False&panels=&refresh=False"
+        src="https://apps.ce.bktencent.com/bk-vision/embed/?uid=3mu6wqHieVcLFhAygQ3R5i&bk_app_id=&bk_app_list=['bk-scut-course']&name=&show_copyright=True&watermark=True&time_readonly=False&show_time=True&show_refresh=True&start_time=now/d&end_time=now/d&preview=False&hide_toolbox=False&hide_filter=False&panels=&refresh=False"
         style="height: 100%;width: 100%;border: 1px solid rgb(220, 222, 229);">
     </iframe>
 
