08c0cb82fc4f9ec9ccc946874cb7517092e63c15
ctenetliu <ctenetliu@tencent.com>
2024-04-26 15:56:18 +0800

    feat: SCUT 迭代二

--- src/store/modules/example.js
+++ src/store/modules/example.js
@@ -46,5 +46,22 @@ export default {
       const url = `https://apps.ce.bktencent.com/stag--default--bk-scut-course/host-detail?${queryString.stringify(params)}`;
       return http.get(url, {}, config);
     },
+    // 根据传入的主机ID列表、文件目录和文件后缀，查询主机上符合条件的文件
+    searchFile(context, params, config = {}) {
+      // eslint-disable-next-line no-undef
+      const url = `https://apps.ce.bktencent.com/stag--default--bk-scut-course/search-file?${queryString.stringify(params)}`;
+      return http.get(url, {}, config);
+    },
+    // 根据传入的主机ID和文件信息，备份符合要求的文件到指定目录
+    backupFile(context, params, config = {}) {
+      // eslint-disable-next-line no-undef
+      const url = `https://apps.ce.bktencent.com/stag--default--bk-scut-course/backup-file?${queryString.stringify(params)}`;
+      return http.get(url, {}, config);
+    },
+    // 查询备份记录
+    getBackupRecord(context, params, config = {}) {
+      // eslint-disable-next-line no-undef
+      return http.get(`https://apps.ce.bktencent.com/stag--default--bk-scut-course/backup-record`, params, config);
+    },
   },
 };
